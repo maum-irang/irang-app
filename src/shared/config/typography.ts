645f6519692805ea8e1a850779f3405a2e165752
shinleehyeon <shinlee7878@gmail.com>
2025-07-07 09:27:07 +0900

    Add color and typography configuration files

---
+++ src/shared/config/typography.ts
@@ -0,0 +1,46 @@
+export const typography = {
+    name: {
+      fontSize: '32px',
+      lineHeight: '48px',
+      fontWeight: 'normal',
+    },
+    largeTitle: {
+      fontSize: '24px',
+      lineHeight: '36px',
+      fontWeight: 'normal',
+    },
+    title: {
+      fontSize: '20px',
+      lineHeight: '30px',
+      fontWeight: 'normal',
+    },
+    caption: {
+      fontSize: '13px',
+      lineHeight: '20px',
+      fontWeight: 'normal',
+    },
+    label: {
+      strong: {
+        fontSize: '18px',
+        lineHeight: '27px',
+        fontWeight: 'bold',
+      },
+      default: {
+        fontSize: '18px',
+        lineHeight: '27px',
+        fontWeight: 'normal',
+      },
+    },
+    body: {
+      strong: {
+        fontSize: '15px',
+        lineHeight: '23px',
+        fontWeight: 'bold',
+      },
+      default: {
+        fontSize: '15px',
+        lineHeight: '23px',
+        fontWeight: 'normal',
+      },
+    },
+  } as const;
