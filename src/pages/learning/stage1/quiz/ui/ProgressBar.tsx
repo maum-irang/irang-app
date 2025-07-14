9b52eeec6232dce21701b4d61764c7fbc673c1c9
shinleehyeon <shinlee7878@gmail.com>
2025-07-15 08:49:31 +0900

    ProgressBar 컴포넌트의 불필요한 줄 삭제 및 코드 정리; UI 일관성 향상

--- src/pages/learning/stage1/quiz/ui/ProgressBar.tsx
+++ src/pages/learning/stage1/quiz/ui/ProgressBar.tsx
@@ -11,8 +11,7 @@ export const ProgressBar: React.FC<ProgressBarProps> = ({
 }) => {
   return (
     <div className="w-full mt-10 mb-5">
-      <div className="flex justify-center items-center mb-2">
-      </div>
+      <div className="flex justify-center items-center mb-2"></div>
       <div className="w-full bg-white rounded-full h-5 shadow-inner">
         <div
           className="h-5 rounded-full transition-all duration-300 bg-purple-400"
